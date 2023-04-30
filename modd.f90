46d42824dda9320b6918ed0105c4ee5b088cc638
Polina <103999174+polinanovy@users.noreply.github.com>
2023-04-30 10:53:23 +0300

    Updated modd

--- modd.f90
+++ modd.f90
@@ -8,9 +8,11 @@ subroutine InitializeParameters(D, a, b, u_left, u_right, N, t_stop)
 end subroutine InitializeParameters
 
 
-subroutine Allocation(N, u_old)
+subroutine Allocation(N,u)
 !Subroutine for allocating memory to dynamic arrays
-
+integer :: N
+real(8) :: u(0:N-1)
+allocate (u(0:N-1))
 end subroutine Allocation
 
 
@@ -44,8 +46,28 @@ end subroutine SetBC
 
 
 subroutine Step(N, u_old, u_new)
+real(8) :: D
+real(8) :: a, b
+real(8) :: u_left, u_right
+real(8) :: N
+
+real(8), allocatable :: u_old(:), u_new(:)
+
+real(8) :: dx, dt
+
+real(8) :: t, t_stop
+
+call FirstStep() !ту на выходе получается значение u (?), оно для схемы Д-Ф будет u_old
+u_old = u !array for holding values 1 step back in time
+
+t = t + dt
 
 end subroutine Step
+
+
+subroutine SaveData(N, u_new)
+
+end subroutine SaveData
 
 
 end module
