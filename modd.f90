44f33eef852e573db26253c3f6d8a724bf9a01d3
Polina <103999174+polinanovy@users.noreply.github.com>
2023-04-30 13:27:08 +0300

    Added Step subroutine

--- modd.f90
+++ modd.f90
@@ -18,21 +18,18 @@ end subroutine Allocation
 
 subroutine InitializeGrid(N, a, b, dx, x)
 !Subroutine for initialising a grid
-
 integer :: a, b, N, i
 real(8) :: x(0:N-1)
 real(8) :: dx
-
 dx = (b - a) / (N - 1)
 x(0) = a; x(N-1) = b
 do i = 1, N-2
  x(i) = x(i-1) + dx
 enddo
-
 end subroutine InitializeGrid
 
 
-subroutine ISetIC(N, u_old)
+subroutine SetIC(x, u_0)
 !Subroutine for setting the initial condition
 
 end subroutine SetIC
@@ -55,23 +52,13 @@ subroutine SetBC(N, a, b, u_old)
 end subroutine SetBC
 
 
-subroutine Step(N, u_old, u_new)
-real(8) :: D
-real(8) :: a, b
-real(8) :: u_left, u_right
-real(8) :: N
-
-real(8), allocatable :: u_old(:), u_new(:)
-
-real(8) :: dx, dt
-
-real(8) :: t, t_stop
-
-call FirstStep() !ту на выходе получается значение u (?), оно для схемы Д-Ф будет u_old
-u_old = u !array for holding values 1 step back in time
-
-t = t + dt
-
+subroutine Step(u,dt,D,dx,N,u_old)
+!Time step according to the DuFort-Frankel scheme
+real(8) :: D, dt, dx, 2c
+integer :: N
+real(8) :: u(
+2c = (2*D*dt)/dx**2
+u(1:N−2) = ((1−2c)*u_old(1:N−2) + 2c*(u(2:N-1) + u(0:N−3))) / (1+2c)
 end subroutine Step
 
 
